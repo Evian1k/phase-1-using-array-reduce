005306d06ac4bcbb25d2002b2bd4302ce55e8c3f
Evian1k <emmanuelevian@gmail.com>
2025-03-25 22:57:05 +0300

    Adds update

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+
 
 // Code your solution here
+
+const batteryBatches = [3, 5, 3, 4, 4, 2, 3, 2, 2, 3]; // Sum = 31
+
+const totalBatteries = batteryBatches.reduce((sum, batch) => sum + batch, 0);
+
+module.exports = { totalBatteries };
